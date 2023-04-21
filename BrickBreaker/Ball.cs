b9a8a8dda4dcbd0b48b8ea12e1edfa4318199917
JellyJamDev <jamejohn2005@gmail.com>
2023-04-21 09:01:58 -0400

    Ball still doesnt work fully but heres my current work

--- BrickBreaker/Ball.cs
+++ BrickBreaker/Ball.cs
@@ -33,9 +33,6 @@ namespace BrickBreaker // HELLO THERE THIS IS A COMMIT TEST FROKM JAMES IF YOU S
 
             if (ballRec.IntersectsWith(blockRec))
             {
-
-
-
                 // Get the range of specific points it may hit
                 if (b.x <= x - size + (size / 8) || b.x >= x + size - (size / 8)) // Hits either side
                 {
