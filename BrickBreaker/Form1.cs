aa07b1cd607b5b89ac2b4bb0d85e298ddcc0e42d
justmorr897 <justmorr897@ed.amdsb.ca>
2023-05-09 09:21:29 -0400

    Form1 Change Screen

--- BrickBreaker/Form1.cs
+++ BrickBreaker/Form1.cs
@@ -32,5 +32,28 @@ namespace BrickBreaker
 
             ms.Location = new Point((this.Width - ms.Width) / 2, (this.Height - ms.Height) / 2);
         }
+
+        public static void ChangeScreen(object sender, UserControl next)
+        {
+            Form f; // will either be the sender or parent of sender
+
+            if (sender is Form)
+            {
+                f = (Form)sender;
+            }
+            else
+            {
+                UserControl current = (UserControl)sender;
+                f = current.FindForm();
+                f.Controls.Remove(current);
+            }
+
+            next.Location = new Point((f.ClientSize.Width - next.Width) / 2,
+                (f.ClientSize.Height - next.Height) / 2);
+
+            f.Controls.Add(next);
+            next.Focus();
+        }
+
     }
 }
